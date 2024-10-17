ab2ba8fcefd9899ce32682766e5018a06acd7768
Brion <40612857+Drazzot@users.noreply.github.com>
2024-10-17 06:52:04 -0400

    rewrite

--- Ch4Ex23_CSC221_Assignment.cpp
+++ Ch4Ex23_CSC221_Assignment.cpp
@@ -16,30 +16,109 @@ Requirements :
  */
 
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
+
+void displayMenu();
+void selectMenu();
+void circleArea();
+void rectangleArea();
+void triangleArea();
+double checkNegative(double inputNumber);
+bool makingEntries = 1;
 
 int main()
 {
-    const double PIE = 3.14;
-    int userSelection;
-    double circleRadius;
+    while (makingEntries == 1) {
+
+        cout << "Welcome to the Geometry Calculator!\n" << endl;
+        displayMenu();
+        selectMenu();
+        cout << "Please make another selection.\n" << endl;
+    }
+    return 0;
+}
+
+void displayMenu() {
+    cout << "Please select one of the following menu items" << endl;
+    cout << "---------------------------------------------" << endl;
+    cout << "To Calculate the area of a circle enter the number 1" << endl;
+    cout << "To Calculate the area of a rectangle enter the number 2" << endl;
+    cout << "To Calculate the area of a right triangle enter the number 3" << endl;
+    cout << "To exit the program enter the number 4" << endl;
+}
     
-    cout << "display the menu";
+void selectMenu() {
+    int userSelection;
 
     cin >> userSelection;
-
     while ((userSelection < 0) || userSelection > 4) {
         cout << "You have made an invalid entry, please try again.\n";
         cin.clear();
         cin >> userSelection;
     }
-
-    if (userSelection == 1) {
-        cout << "Please enter the radius for the circle:\n";
-        cin >> circleRadius;
-        cout << "The area of the cirle is: " << PIE * (pow(circleRadius, 2));
-
+    switch (userSelection) {
+    case 1:
+        circleArea();
+        break;
+    case 2:
+        circleArea();
+        break;
+    case 3:
+        circleArea();
+        break;
+    case 4:
+        cout << "Exiting the program.\n";
+        exit(0);
     }
+}
+    
+void circleArea() {
+    const double PIE = 3.14;
+    double circleRadius;
 
-    return 0;
+    cout << fixed;
+    cout << "Please enter the radius for the circle:\n";
+    cin >> circleRadius;
+    checkNegative(circleRadius);
+    cout << "The area of the cirle is: " << PIE * (pow(circleRadius, 2));
+}
+
+void rectangleArea() {
+    double length;
+    double width;
+    
+    cout << fixed;
+    cout << "Please enter the length of the rectangle:\n";
+    cin >> length;
+    checkNegative(length);
+    cout << "Please enter the width of the rectangle:\n";
+    cin >> width;
+    checkNegative(width);
+    cout << "The area of the rectangle is: " << length * width << endl;
+}
+
+void triangleArea() {
+    double base;
+    double height;
+    
+    cout << fixed;
+    cout << "Please enter the base of the right triangle:\n";
+    cin >> base;
+    checkNegative(base);
+    cout << "Please enter the height of the right triagle:\n";
+    cin >> height;
+    checkNegative(height);
+    cout << "The area of the right triangle is: " << base * height * .5 << endl;
+}
+
+double checkNegative(double inputNumber) {
+    while ((inputNumber < 0)) {
+        cout << "You have entered a negative number, please enter a positive number instead.\n";
+        cin.clear();
+        cin >> inputNumber;
+        return inputNumber;
+    }
+    return inputNumber;
 }
